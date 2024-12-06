e7f084a3a9045bd1192e2bf64a753e7ed6a55d91
Jakob Theopold <jtheopold@lcs.on.ca>
2024-12-06 14:26:09 -0500

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Jakob Theopold on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

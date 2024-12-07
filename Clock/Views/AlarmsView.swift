8613eeae64780dfee8e12488d1b02eda932e4e76
Jayden Kim <hkim@lcs.on.ca>
2024-12-07 15:02:29 -0500

    everything but edit and +

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -11,23 +11,52 @@ struct AlarmsView: View {
     var body: some View {
         NavigationStack{
             
-            VStack{
+            VStack(alignment:. leading) {
+                HStack(alignment: .bottom) {
+                   
+                    
+                    Text("")
+                    
+                    Image(systemName: "bed.double.fill")
+                        .font(.headline)
+                    
+                    
+                    Text("Sleep | Wake Up")
+                        .font(.headline)
+                }
+                
+                
                 Divider()
+                
+                
                 HStack {
                     
-                    Text("Other")
-                        .font(.headline)
+                    
+                    
+                    Text("No Alarm")
+                        .foregroundStyle(Color.gray)
+                    
                         .padding()
                     
+                    Text("                                    ")
                     
-                    Button("Capsule") { }
-                    .buttonBorderShape(.capsule)
-                    .buttonStyle(.borderedProminent)
+                    
+                    Button(action: {}) {
+                        Text("SET UP")
+                            .padding(EdgeInsets(top: 4, leading: 9, bottom: 4, trailing: 9))
+                            .background(Color.setUpGray, in: Capsule())
+                    }
                     
                     
                     
                     
                 }
+                Divider()
+                
+                Text(" ")
+                Text("  Other")
+                    .font(.headline)
+                
                 Divider()
                 
                 List {
@@ -36,7 +65,7 @@ struct AlarmsView: View {
                     SingleAlarmView(time: "6:30", isOn: true, AMOrPM: "PM")
                     SingleAlarmView(time: "5:30", isOn: false, AMOrPM: "AM")
                     SingleAlarmView(time: "4:30", isOn: true, AMOrPM: "PM")
-
+                    
                 }
             }
             .listStyle(.plain)

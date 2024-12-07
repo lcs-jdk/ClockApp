43d370c8f3481a99f4cae0f2c43ffcc82616846e
Jayden Kim <hkim@lcs.on.ca>
2024-12-07 11:00:40 -0500

    got my divider

--- Clock/Views/AlarmsView.swift
+++ Clock/Views/AlarmsView.swift
@@ -11,13 +11,33 @@ struct AlarmsView: View {
     var body: some View {
         NavigationStack{
             
-            
-            List {
-                SingleAlarmView(time: "7:30", isOn: true, AMOrPM: "AM")
-                SingleAlarmView(time: "6:30", isOn: true, AMOrPM: "PM")
-                SingleAlarmView(time: "5:30", isOn: false, AMOrPM: "AM")
-                SingleAlarmView(time: "4:30", isOn: true, AMOrPM: "PM")
+            VStack{
+                Divider()
+                HStack {
+                    
+                    Text("Other")
+                        .font(.headline)
+                        .padding()
+                    
+                    
+                    Button("Capsule") { }
+                    .buttonBorderShape(.capsule)
+                    .buttonStyle(.borderedProminent)
+                    
+                    
+                    
+                    
+                }
+                Divider()
+                
+                List {
+                    
+                    SingleAlarmView(time: "7:30", isOn: true, AMOrPM: "AM")
+                    SingleAlarmView(time: "6:30", isOn: true, AMOrPM: "PM")
+                    SingleAlarmView(time: "5:30", isOn: false, AMOrPM: "AM")
+                    SingleAlarmView(time: "4:30", isOn: true, AMOrPM: "PM")
 
+                }
             }
             .listStyle(.plain)
             .navigationTitle("Alarms")
